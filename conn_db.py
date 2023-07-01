3950b4f352db153e1cd9b9293011bd5ea03b3d27
白楚き <haku@shirosokinoMacBook-Air.local>
2023-07-02 02:37:25 +0900

    add new feature which can backup product inventory when fetch new

--- conn_db.py
+++ conn_db.py
@@ -3,14 +3,14 @@ from mysql.connector import Error
 
 
 class MySQL():
-    def __init__(self, table) -> None:
+    def __init__(self, database) -> None:
         # Establish a connection to the MySQL database
         self.cnx = mysql.connector.connect(
             host='18.179.195.219',  # Replace with your MySQL host
             user='zag_shop',  # Replace with your MySQL username
             port='3308',
             password='DYr36i3P5Bz2Mzpj',  # Replace with your MySQL password
-            database=table  # Replace with your MySQL database name
+            database=database  # Replace with your MySQL database name
         )
 
         # Create a cursor object
@@ -36,8 +36,28 @@ class MySQL():
         finally:
             if self.cursor or self.cnx:
                 self.close()  
+    def backup_product_inventory(self):
+        try:
+            insert_query = """
+            INSERT INTO zag_product_inventory_history_bk (product_id, is_available, price, is_take_off, take_off_time, update_time)
+            SELECT product_id, is_available, price, is_take_off, take_off_time, update_time from zag_product_inventory_history
+            """
+            self.cursor.execute(insert_query)
+            self.cnx.commit()
+            print("backup successfully")
+        except Error as e:
+            print("backup insert error")
+        finally:
+            print("clear table zag_product_inventory_history data")
+            self.cursor.execute("DELETE FROM zag_product_inventory_history")
+            self.cnx.commit()
+            if self.cursor or self.cnx:
+                self.close()
 
     def close(self):
         # Close the cursor and connection
         self.cursor.close()
         self.cnx.close()
+
+db = MySQL("zag_shop")
+db.backup_product_inventory()
